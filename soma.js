8e89694c4741505d0c4be89499c021b83f123f1c
wbhaese <wbhaese@gmail.com>
2020-06-08 11:42:21 -0300

    Add contents of classes 2 of ECMAScript

---
+++ soma.js
@@ -0,0 +1,3 @@
+// export default function soma(a, b) {//com export defautl, pode-se usar uma expressão qualquer ao importar
+//     return a + b;
+// }
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// import { soma as somaFunction, sub } from './funcoes'; //renomear a function
+// import soma from './soma';
+// import soma, { sub } from './funcoes';//IMportando função padrão e função normais
+// import * as funcoes from './funcoes';//importando todas as funções numa só variável
+
+// console.log('soma ',funcoes.soma( 2, 4));
+// console.log('subtração ', funcoes.sub(6, 4));
+
+//-----------------------
+
+//aula2.3
+
+//webserver monitora as modificações e atualiza no front
+// alert('testando1')
+
+
+//aula 3.1
+
